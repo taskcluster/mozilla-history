4f3986027b299b4099a7387f5711c544f659cd41
Pete Moore <pmoore@mozilla.com>
2020-06-04 12:07:55 +0200

    Bump taskcluster go client version

--- audit-worker-versions/main.go
+++ audit-worker-versions/main.go
@@ -15,9 +15,9 @@ import (
 
 	"github.com/taskcluster/httpbackoff/v3"
 	"github.com/taskcluster/slugid-go/slugid"
-	tcclient "github.com/taskcluster/taskcluster/v29/clients/client-go"
-	"github.com/taskcluster/taskcluster/v29/clients/client-go/tcqueue"
-	"github.com/taskcluster/taskcluster/v29/clients/client-go/tcworkermanager"
+	tcclient "github.com/taskcluster/taskcluster/v30/clients/client-go"
+	"github.com/taskcluster/taskcluster/v30/clients/client-go/tcqueue"
+	"github.com/taskcluster/taskcluster/v30/clients/client-go/tcworkermanager"
 )
 
 type (
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/taskcluster/mozilla-history
 go 1.13
 
 require (
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/taskcluster/httpbackoff/v3 v3.1.0
 	github.com/taskcluster/slugid-go v1.1.0
-	github.com/taskcluster/taskcluster/v29 v29.3.0
+	github.com/taskcluster/taskcluster/v30 v30.0.2
 )
